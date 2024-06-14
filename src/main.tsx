36fe5d2120d4c7d9d93f957ac2b582541d461613
charlieop <spchanae@connect.ust.hk>
2024-06-14 14:07:53 +0800

    fixed a ts error in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 // import reportWebVitals from './reportWebVitals';
 
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json"
+import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
