7e544118412a5d6e4b18bed06b9444409ff6c5ec
charlieop <spchanae@connect.ust.hk>
2024-06-14 14:20:49 +0800

    added amplify backend

---
+++ amplify/auth/resource.ts
@@ -0,0 +1,37 @@
+import { defineAuth } from "@aws-amplify/backend";
+
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+  userAttributes: {
+    email: {
+      required: true,
+      mutable: true,
+    },
+    phoneNumber: {
+      required: false,
+      mutable: true,
+    },
+    preferredUsername: {
+      required: false,
+      mutable: true,
+    },
+    address: {
+      required: true,
+      mutable: true,
+    },
+    familyName: {
+      required: true,
+      mutable: true,
+    },
+    givenName: {
+      required: true,
+      mutable: true,
+    },
+  },
+});
