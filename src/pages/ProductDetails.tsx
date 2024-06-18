be3c961fcdaabfd2555a85c9c64cf97bf00b3af5
charlieop <spchanae@connect.ust.hk>
2024-06-18 19:02:24 +0800

    updated product page

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -3,6 +3,7 @@ import BreadCrumps from "../components/BreadCrumps";
 import { useState, useEffect } from "react";
 import type { Schema } from "../../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { Button } from "@aws-amplify/ui-react";
 
 const client = generateClient<Schema>({
   authMode: "identityPool",
@@ -16,18 +17,112 @@ function ProductDetails() {
     const num = Math.floor(Math.random() * items.length);
     setProduct(items[num]);
   };
+
+  function generateRandomWords() {
+    const words = [
+      "lorem",
+      "ipsum",
+      "dolor",
+      "sit",
+      "amet",
+      "consectetur",
+      "adipiscing",
+      "elit",
+      "sed",
+      "do",
+      "eiusmod",
+      "tempor",
+      "incididunt",
+      "ut",
+      "labore",
+      "et",
+      "dolore",
+      "magna",
+      "aliqua",
+      "ut",
+      "enim",
+      "ad",
+      "minim",
+      "veniam",
+      "quis",
+      "nostrud",
+      "exercitation",
+      "ullamco",
+      "laboris",
+      "nisi",
+      "ut",
+      "aliquip",
+      "ex",
+      "ea",
+      "commodo",
+      "consequat",
+      "duis",
+      "aute",
+      "irure",
+      "dolor",
+      "in",
+      "reprehenderit",
+      "in",
+      "voluptate",
+      "velit",
+      "esse",
+      "cillum",
+      "dolore",
+      "eu",
+      "fugiat",
+      "nulla",
+      "pariatur",
+      "excepteur",
+      "sint",
+      "occaecat",
+      "cupidatat",
+      "non",
+      "proident",
+      "sunt",
+      "in",
+      "culpa",
+      "qui",
+      "officia",
+      "deserunt",
+      "mollit",
+      "anim",
+      "id",
+      "est",
+    ];
+    return words[Math.floor(Math.random() * words.length)];
+  }
+
+  function generateRandomParagraph(words: number) {
+    let paragraph = "";
+    for (let i = 0; i < words; i++) {
+      paragraph += generateRandomWords() + " ";
+    }
+    return paragraph;
+  }
+
+  function randInt(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
   const createProduct = async () => {
     const letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const a = await client.models.Product.create(
       {
         name: "New Product Type-" + letter[Math.floor(Math.random() * 26)],
-        price: 100,
-        code: "testCode",
+        price: randInt(100, 1000),
+        code:
+          "testCode-" +
+          letter[Math.floor(Math.random() * 26)] +
+          letter[Math.floor(Math.random() * 26)] +
+          letter[Math.floor(Math.random() * 26)],
         version: "1." + Math.random().toFixed(3),
-        dimension: "10x10",
-        description: "This is a new product",
-        requirement: "This is a requirement",
-        remark: "This is a remark",
+        dimension:
+          randInt(10, 100) + "x" + randInt(10, 100) + "x" + randInt(10, 100),
+        description:
+          "This is a new product: " + generateRandomParagraph(randInt(30, 40)),
+        requirement:
+          "This is a requirement: " + generateRandomParagraph(randInt(10, 20)),
+        remark: "This is a remark: " + generateRandomParagraph(randInt(15, 30)),
       },
       {
         authMode: "userPool",
@@ -35,29 +130,80 @@ function ProductDetails() {
     );
     console.log(a);
   };
+
+  const switchProduct = async () => {
+    await fetchProduct();
+  };
+
+  const deleteProduct = async () => {
+    if (product) {
+      const a = await client.models.Product.delete({
+        id: product.id,
+      });
+      console.log(a);
+      setProduct(undefined);
+    }
+  };
+
   useEffect(() => {
     fetchProduct();
   }, []);
+
   return (
     <>
       <BreadCrumps page="product-details" title="Product Details" />
       <section className="productDetail-wrapper">
-        <h1>{product?.name ?? "No Product Info Found :("}</h1>
-        {product && (
-          <>
-            <p>
-              {product?.code} version - {product?.version}
-            </p>
-            <br></br>
-            <h2>Description</h2>
-            <p>{product?.description}</p>
-            <br></br>
-            <h2>Requirements</h2>
-            <p>{product?.requirement}</p>
-          </>
-        )}
-        <br></br> <br></br> <br></br>
-        <button onClick={createProduct}>Create a new Product</button>
+        <div className="grid-col">
+          <h1>{product?.name ?? "No Product Info Found :("}</h1>
+          {product && (
+            <>
+              <p>
+                {product?.code} version - {product?.version}
+              </p>
+              <div className="group">
+                <h2>Description</h2>
+                <p>{product?.description}</p>
+              </div>
+              <div className="group">
+                <h2>Requirements</h2>
+                <p>{product?.requirement}</p>
+              </div>
+              <div className="group">
+                <h2>Price</h2>
+                <p>{product?.price} HKD</p>
+              </div>{" "}
+              <div className="group">
+                <h2>Dimension</h2>
+                <p>{product?.dimension}</p>
+              </div>{" "}
+              <div className="group">
+                <h2>Remark</h2>
+                <p>{product?.remark}</p>
+              </div>{" "}
+            </>
+          )}
+        </div>
+        <div className="grid-col">
+          <img src="https://placehold.co/400x300" alt="" />
+          <Button
+            className="detail-btn"
+            onClick={createProduct}
+            colorTheme="success"
+          >
+            Create a new Product
+          </Button>
+          <Button className="detail-btn" onClick={switchProduct}>
+            Switch to a different Product
+          </Button>
+          <Button
+            className="detail-btn"
+            onClick={deleteProduct}
+            colorTheme="error"
+            variation="primary"
+          >
+            Delete this Product
+          </Button>
+        </div>
       </section>
     </>
   );
